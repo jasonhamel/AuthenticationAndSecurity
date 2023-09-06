37af566138611ff07660ac8771ce6f2bee41eb56
Jason <github@jasongetsemail.com>
2023-09-05 20:01:27 -0400

    Setup express, ejs, and body-parser. Setup server to listen and serve /, /login, and /register

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
-//jshint esversion:6
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.listen("3000", () => {
+  console.log("App started on port 3000");
+});
