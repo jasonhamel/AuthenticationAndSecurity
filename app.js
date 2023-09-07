b9b9edd613e0fdbdb553992eb8e7b9a4ed6a03ca
Jason <github@jasongetsemail.com>
2023-09-07 19:03:58 -0400

    Add and configure mongoose-encryption package. Took the extra step of adding dotenv to hide the secret.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
+import encrypt from "mongoose-encryption";
+import "dotenv/config";
 
 const app = express();
 app.use(express.static("public"));
@@ -9,10 +11,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
-const userSchema = {
+const secret = process.env.SECRET;
+
+const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-};
+});
+
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,8 +50,6 @@ app.post("/login", (req, res) => {
 });
 
 app.post("/register", (req, res) => {
-  console.log(req.body.username);
-  console.log(req.body.password);
   const newUser = new User({
     username: req.body.username,
     password: req.body.password,
