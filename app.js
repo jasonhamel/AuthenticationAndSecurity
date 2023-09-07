a8f01447b7af86223aa231e98a540d1f7614fd0f
Jason <github@jasongetsemail.com>
2023-09-07 18:43:04 -0400

    Add mongoose and a way to store/read login info

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const app = express();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = {
+  username: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -16,6 +26,38 @@ app.get("/login", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/login", (req, res) => {
+  const enteredUsername = req.body.username;
+  const enteredPassword = req.body.password;
+  User.findOne({ username: enteredUsername })
+    .then((foundUser) => {
+      if (foundUser.password === enteredPassword) {
+        res.render("secrets");
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.render("login");
+    });
+});
+
+app.post("/register", (req, res) => {
+  console.log(req.body.username);
+  console.log(req.body.password);
+  const newUser = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+  newUser
+    .save()
+    .then(() => {
+      res.render("secrets");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.listen("3000", () => {
